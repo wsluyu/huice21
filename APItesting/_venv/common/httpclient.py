19b18fd772e4a0a2e9773899386782afd05bdf7c
luyu <renren@RENRENdeMacBook-Air-4.local>
2021-08-27 18:55:38 +0800

    get接口封装

--- APItesting/_venv/common/httpclient.py
+++ APItesting/_venv/common/httpclient.py
@@ -9,8 +9,8 @@ def get():
 def put():
     requests.put()
 # 请求是独立的
-get()
-put()
+# get()
+# put()
 
 
 class HttpClient:
@@ -18,15 +18,25 @@ class HttpClient:
     def __init__(self):
         self.__session = requests.session()
 
-    def get(self):
-        self.__session.get()
+    def get(self, url, params=None, cookies=None, timeout=None):
+        response = self.__session.get(url=url, params=params, cookies=cookies, timeout=timeout)
+        if response.status_code == 301:
+            response = self.__session.get(url=response.headers.get('location'))
+        try:
+
+            return response.status_code, response.json()
+
+        except:
+            # json解析错误
+            pass
+            # 写到日志中
 
     def put(self):
         self.__session.put()
 
     def post(self):
         self.__session.post()
-
+'''
 # 类里面的请求共用一个会话，如一个登陆账号
 # 如账号1
 client = HttpClient()
@@ -36,3 +46,10 @@ client.post()
 client2 = HttpClient()
 client2.get()
 client2.post()
+'''
+if __name__ == '__main__':
+    client = HttpClient()
+    params = {'status': 'available'}
+    s, k = client.get(url='https://petstore.swagger.io/v2/pet/findByStatus', params=params)
+    print(s, k)
+
