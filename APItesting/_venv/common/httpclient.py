e668bd17903ce596bcdae9d362c8b894d9383d41
luyu <renren@RENRENdeMacBook-Air-4.local>
2021-09-02 18:55:45 +0800

    1.verify=False环境解决报错问题 2.添加post请求

--- APItesting/_venv/common/httpclient.py
+++ APItesting/_venv/common/httpclient.py
@@ -14,6 +14,7 @@ def put():
 # get()
 # put()
 
+# 封装response返回数据
 class Response:
     def __init__(self, response):
         self.__response = response
@@ -46,7 +47,7 @@ class HttpClient:
         try:
 
             # return response.status_code, response.json()
-            return Response(response)
+            return Response(response).data
         except:
             # json解析错误
             pass
@@ -55,9 +56,10 @@ class HttpClient:
     def put(self):
         self.__session.put()
 
-    def post(self, url, data=None, headers=None):
-        response = self.__session.post(url=url, data=data, headers=headers)
-        return response.status_code, response.content
+    def post(self, url, data=None, headers=None, cookies=None, timeout=None, verify=False):
+            response = self.__session.post(url=url, data=data, headers=headers, cookies=cookies, timeout=timeout, verify=verify)
+            return Response(response).data
+            # return response.status_code
 
 
 '''
@@ -85,11 +87,27 @@ dataS = '''
 
 if __name__ == '__main__':
     client = HttpClient()
-    params = {'status': 'available'}
-    s, k = client.get(url='https://petstore.swagger.io/v2/pet/findByStatus', params=params)
+    # params = {'status': 'available'}
+    # s, k = client.get(url='https://petstore.swagger.io/v2/pet/findByStatus', params=params)
     # print(s, k)
-    headers = {'content-type': 'application/json'}
-    a, b = client.post(url='https://petstore.swagger.io/v2/store/order', data=dataS, headers=headers)
-    print(a, b)
 
+    # headers = {'content-type': 'application/json'}
+    # a = client.post(url='https://petstore.swagger.io/v2/store/order', data=dataS, headers=headers, verify=False)
+    # print(a)
+
+
+    # 获取登录Token
+    user = {'username': 'admin', 'password': 'admin'}
+    response = client.post(url='http://flash-admin.enilu.cn/prod-api/account/login', data=user, verify=False)
+
+    print(response)
+    # 获取返回的字典中的，token数据
+    token = response.get('data').get('data').get('token')
+    print(token)
+
+    # 部门管理添加部门（需要登录）
+    headers = {'Authorization': token}
+    d = client.post(url='http://flash-admin.enilu.cn/prod-api/dept?simplename=xxx&fullname=xy&num=123&pid=1', headers=headers)
+    print(d)
 #  问题：post请求，传参是json数据，为什么要用data而不用json??? #
+
